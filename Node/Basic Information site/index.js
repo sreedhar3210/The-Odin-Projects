98c3d1c5cd805af43d3ad80f1a68ff566c5bec41
sreedhar3210 <sreedhar.palampalli@gmail.com>
2024-11-15 19:01:01 +0530

    done with express

--- Node/Basic Information site/index.js
+++ Node/Basic Information site/index.js
@@ -1,29 +1,20 @@
-var http = require('http');
-var fs = require('fs');
+
+var express = require('express');
+var app = express();
 
 const port = 8080;
-var httpFunction = function (req,res) {
-    var displayFile = '';
-    if(req.url == '/')                      displayFile='index.html';
-    else if(req.url == '/about')            displayFile='about.html';
-    else if(req.url == '/contact-me')       displayFile='contact-me.html';
-    else                                    displayFile='404.html';
-    fs.readFile(displayFile, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.write("Internal Server Error");
-            res.end();
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-        }
-    });
-}
 
 var connectedFunction = function (){
     console.log("connected to the port " + port + " succesfully");
 }
-var server = http.createServer(httpFunction);
 
-server.listen(8080,connectedFunction);
+app.get('/*', function (req, res) {
+    var outputHtml;
+    if(req.url == '/')                   outputHtml = '/index.html';
+    else if(req.url == '/about')         outputHtml = '/about.html';
+    else if(req.url == '/contact-me')    outputHtml = '/contact-me.html';
+    else                                 outputHtml = '/404.html';
+    res.sendFile(__dirname + outputHtml);
+})
+
+app.listen(8080,connectedFunction);
